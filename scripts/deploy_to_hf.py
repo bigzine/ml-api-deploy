431f7f4ebad9e2d5f6f767d369a84a4618a9e337
ediagabate <bigzine@gmail.com>
2026-06-07 16:00:16 +0200

    ci:  update deploy file

--- scripts/deploy_to_hf.py
+++ scripts/deploy_to_hf.py
@@ -1,31 +1,34 @@
 """
 Déploiement automatique vers Hugging Face Spaces.
-Appelé par le pipeline CD.
 """
 import os
 from huggingface_hub import HfApi
 
 HF_TOKEN   = os.environ["HF_TOKEN"]
-HF_REPO_ID = os.environ.get("HF_SPACE_ID", "votre-username/ml-api-deploy")
+HF_REPO_ID = os.environ["HF_SPACE_ID"]  # ex: Ediagabate/ml-api-deploy
 
 FILES_TO_UPLOAD = [
+    "app/__init__.py",
     "app/main.py",
     "app/model.py",
     "app/schemas.py",
-    "app/routers/predict.py",
     "app/routers/__init__.py",
-    "app/__init__.py",
+    "app/routers/predict.py",
     "requirements.txt",
     "Dockerfile",
-    "README.md",
+    "README_SPACES.md",
 ]
 
 api = HfApi(token=HF_TOKEN)
 
 for filepath in FILES_TO_UPLOAD:
+    path_in_repo = filepath
+    if filepath == "README_SPACES.md":
+        path_in_repo = "README.md"
+
     api.upload_file(
         path_or_fileobj=filepath,
-        path_in_repo=filepath,
+        path_in_repo=path_in_repo,
         repo_id=HF_REPO_ID,
         repo_type="space",
     )
